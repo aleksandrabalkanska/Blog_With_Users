e10c5abf33bf7249b78e6937a071606f27612274
thesloth98 <aleksandrabalkanska@gmail.com>
2023-05-23 17:28:21 +0900

    Initial Commit

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ from flask_gravatar import Gravatar
 from random import randint
 from functools import wraps
 from sqlalchemy import Table, Column, Integer, ForeignKey
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")  # '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 login_manager = LoginManager()
@@ -37,7 +38,7 @@ def admin_only(f):
 
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL") #'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
